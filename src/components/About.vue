9a4bee5d6ebb5ed79f22bb368c036cf4e1687314
JYep <jo.yep@cignium.com>
2018-01-02 22:39:03 -0500

    second commit

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,11 +4,27 @@
     <p>
       It's a digital currency in which encryption techniques are used to regulate the generation of units of currency and verify the transfer of funds, operating independently of a central bank.
     </p>
+    <vue-markdown :source="source"></vue-markdown>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'About'
-}
+import VueMarkdown from 'vue-markdown' // production
+  import testmd from './test.md'
+  export default {
+    name: "About",
+    data() {
+      return {
+        source: "# Hello World \n ! Just testing... \n ## Simple Code Block \n ```html <template>  <div>Nothing happends</div></template>```"
+      }
+    },
+    components: {
+    VueMarkdown
+    }/*,
+    mounted() {
+      setInterval(() => {
+        this.source = "#### Table 2 \n aaaa";
+      }, 1000);
+    }*/
+  }
 </script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,70 +1,71 @@
 <template>
   <div>
     <div class="hero-body" style="padding-top: 0">
-
-        <div class="section">
-            <div class="container">
-                <h1 class="title">
-                    Welcome to The Lead Generation Automation Tool
-                </h1>
-                <p class="subtitle">
-                    <strong>Available projects with test automation :</strong>
-                </p>
-                <div class="field has-addons">
-                  <div class="control is-expanded">
-                  <input class="input" v-model="textSearch" type="text" placeholder="Find a project"/>
-                  </div>
-                  <div class="control">
-                    <a class="button is-info">Search</a>
-                  </div>
-                </div> 
+      <div class="section">
+        <div class="container">
+          <h1 class="title">
+            Welcome to The Lead Generation Automation Tool
+          </h1>
+          <p class="subtitle">
+            <strong>Available projects with test automation :</strong>
+          </p>
+          <div class="field has-addons">
+            <div class="control is-expanded">
+              <input class="input" v-model="textSearch" type="text" placeholder="Find a project" />
             </div>
-        </div>
-          <div class="section">
-            <div class="columns is-mobile is-multiline is-centered">
-              <div class="container columns is-multiline is-centered">
-              <div v-for="item in projectsFilter" v-bind:todo="item" :key="item.id" class="column is-narrow">
-                 <router-link :to="`/plans/${item.id}`"  class="box notification is-success">
-                 <p class="title">{{ item.name }} </p>
-                  <p class="subtitle">{{ item.id }}</p>
-                 </router-link>
-              </div>
-              </div>
+            <div class="control">
+              <a class="button is-info">Search</a>
             </div>
           </div>
+        </div>
+      </div>
+      <div class="section">
+        <div class="columns is-mobile is-multiline is-centered">
+          <div class="container columns is-multiline is-centered">
+            <div v-for="item in projectsFilter" v-bind:todo="item" :key="item.id" class="column is-narrow">
+              <router-link :to="`/plans/${item._id}`" class="box notification is-success">
+                <p class="title">{{ item.name }} </p>
+                <p class="subtitle">{{ item._id }}</p>
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
+
+
 <script>
-import axios from 'axios'
-import projectdata from '../mock/projects.json'
-export default {
-  name: 'Hello',
-  data: function () {
-    return{
-    textSearch: "",
-    projects: projectdata
+  import axios from 'axios'
+  import projectdata from '../mock/projects.json'
+  export default {
+    name: 'Hello',
+    data: function() {
+      return {
+        textSearch: "",
+        projects: projectdata
+      }
+    },
+    computed: {
+      projectsFilter: function() {
+        var textSearch = this.textSearch;
+        return this.projects.filter(function(projects) {
+          return projects.name.toLowerCase().indexOf(textSearch.toLowerCase()) !== -1;
+        });
+      }
+    },
+    //call when apis are done
+    created: function() {
+      var that = this
+      axios.get('http://localhost:64482/api/applications')
+        .then(function(response) {
+          that.projects = response.data;
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
     }
-  },
-  computed: {
-     projectsFilter: function() {
-       var textSearch = this.textSearch;
-       return this.projects.filter(function(projects) {
-         return projects.name.toLowerCase().indexOf(textSearch.toLowerCase()) !== -1;
-       });
-     }
-  },
-  created: function() {
-    var that = this
-    axios.get('http://localhost:64482/api/applications')
-    .then(function (response) {
-      that.projects = response.data;
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-    
   }
-}
 </script>
