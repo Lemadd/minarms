b0255d82d03ab2bc774f2e39821a3caf9de22a09
JYep <jo.yep@cignium.com>
2018-01-05 17:57:53 -0500

    remove extra style

--- src/App.vue
+++ src/App.vue
@@ -71,7 +71,7 @@
     >
       <v-toolbar-title :style="$vuetify.breakpoint.smAndUp ? 'width: 300px; min-width: 250px' : 'min-width: 72px'" class="ml-0 pl-3">
         <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-        <span class="hidden-xs-only"><router-link :to="{ name: 'Home' }" style="color: white;text-decoration: none;">Home</router-link></span>
+        <span><router-link :to="{ name: 'Home' }" style="color: white;text-decoration: none;">Home</router-link></span>
       </v-toolbar-title>
 
       <div class="d-flex align-center" style="margin-left: auto">
